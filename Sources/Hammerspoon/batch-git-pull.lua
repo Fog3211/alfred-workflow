e6440324a8d2d518fcb5d1a85272e2efd47fb498
Fog3211 <fog3211@163.com>
2021-09-05 21:27:07 +0800

    feat: batch git pull add listen custom event

--- Sources/Hammerspoon/batch-git-pull.lua
+++ Sources/Hammerspoon/batch-git-pull.lua
@@ -9,29 +9,45 @@
 local lfs = require 'lfs'
 
 -- 只对'Code'和'Mine'目录操作
-local targetGitDir = {'../Test'}
--- local targetGitDir = {'~/Code', '~/Mine'}
+-- local targetGitDir = {os.getenv('HOME')..'../Test'}
+local targetGitDir = {
+    Code = {wifi = 'HUPU.WORK.5G', dir = os.getenv('HOME') .. '/Code'},
+    Mine = {wifi = '*', dir = os.getenv('HOME') .. '/Mine'}
+}
 
 local runGitPullShell = function(filePath)
-    local error1= os.execute('cd ' .. filePath .. '&& git pull --rebase')
-    print(error1)
+    os.execute('cd ' .. filePath .. '&& git pull --rebase --all')
 end
 
-local attrdir = function(rootPath)
-    for subFile in lfs.dir(rootPath) do
-        if subFile ~= '.' and subFile ~= '..' then
-            -- 过滤'.'和'..'目录
-            local subFilePath = rootPath .. '/' .. subFile
-            local subFileAttr = lfs.attributes(subFilePath)
-            if subFileAttr.mode == 'directory' then
-                runGitPullShell(subFilePath)
+local attrdir = function(rootPath, wifi)
+    local currWifi = hs.wifi.currentNetwork()
+    -- 对wifi进行判断，内网才请求工作目录
+    if (currWifi == wifi) or wifi == '*' then
+        for file in lfs.dir(rootPath) do
+            if file ~= '.' and file ~= '..' then
+                -- 过滤'.'和'..'目录
+                local filePath = rootPath .. '/' .. file
+                local fileAttr = lfs.attributes(filePath)
+                if fileAttr.mode == 'directory' then
+                    for subFile in lfs.dir(filePath) do
+                        -- 先检查当前项目是否有.git，没有的话跳过
+                        if subFile == '.git' then
+                            runGitPullShell(filePath)
+                        end
+                    end
+                end
             end
         end
     end
 end
 
 local batchGitPull = function()
-    for index, dir in pairs(targetGitDir) do attrdir(dir) end
+    for _, item in pairs(targetGitDir) do attrdir(item.dir, item.wifi) end
 end
 
-batchGitPull()
+-- 用于和alfred配合做自定义事件监听
+local configFileWatcher = hs.pathwatcher.new(
+                              os.getenv('HOME') ..
+                                  '/.hammerspoon/log/batch-git-pull.log',
+                              batchGitPull)
+configFileWatcher:start()
